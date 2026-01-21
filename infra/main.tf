916ccd2043b5203c3d51ff3119c8e79d0968b038
bienvenue cedrick <cedrick4smith@gmail.com>
2026-01-21 23:29:15 +0200

    infra: add terraform configuration for aws ec2

--- infra/main.tf
+++ infra/main.tf
@@ -1,25 +1,60 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "todo_app" {
-  name         = "todo-app:latest"
-  keep_locally = true
+provider "aws" {
+  region = "us-east-1"
 }
 
-# In a real cloud scenario, this would be an ECS Service or Kubernetes Deployment
-resource "docker_container" "todo_app_container" {
-  image = docker_image.todo_app.image_id
-  name  = "todo-app-terraform"
-  ports {
-    internal = 3000
-    external = 3000
+resource "aws_security_group" "todo_sg" {
+  name_prefix = "todo-app-sg"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "todo_server" {
+  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (us-east-1)
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.todo_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 3000:3000 --name todo-app cedrick13/todo-app:latest
+              EOF
+
+  tags = {
+    Name = "TodoAppServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.todo_server.public_ip
 }
